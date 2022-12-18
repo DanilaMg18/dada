04d13f7f3efef7676fcbc4472594d4aeb0fcdbac
Danila Palcech <dpalcech@gmail.com>
2022-12-18 17:47:06 +0200

    panic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import Logo from "./components/Logo";
+import React, {useState} from "react";
+import ChangeOnOtherBlock from "./components/ChangeBlock";
 
 function App() {
+  let [form,setForm] = useState({
+    username: 'Danila',
+    password: '12345',
+  })
+
+  function handleChange (e) {
+    const {name} = e.target
+    setForm (prev => {
+      return{
+        ...prev,
+        [name] : e.target.value
+      }
+    })
+  }
+
+  function trueFalse(e) {
+    e.preventDefault()
+    if(form.username !== '' && form.password !== '') {
+      trueLogin()
+    }else {
+      alert('Zapolnite')
+    }
+  }
+
+  function trueLogin (e) {
+    if(form.username === document.getElementById('inp1').value && form.password === document.getElementById('inp2').value ) {
+      render()
+    }else {
+      alert('Incorrect')
+    }
+  }
+
+  function render() {
+    return(
+      <ChangeOnOtherBlock/>
+    )
+  }
+
   return (
     <div className="App">
-      <Logo/>
+      <form onSubmit={trueFalse}>
+        <div className="login-block">
+          <input className="Input1" type='text' id="inp1" name='Username' placeholder='Username' onChange={handleChange}/>
+          <input className="Input2" type='text' id="inp2" name='Password' placeholder='Password' onChange={handleChange}/>
+          <button className="button-login">LOGIN</button>
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/components/ChangeBlock.js
@@ -0,0 +1,8 @@
+export default function ChangeOnOtherBlock() {
+  return(
+    <div className="block2">
+      <button className="btn1">List</button>
+      <button className="btn2">Create</button>
+    </div>
+  )
+}
